2b2ac1714d7764910bc1733d5d805b7065bbf05d
kiansierra <kiansierra90@gmail.com>
2024-06-22 15:21:40 +0100

    chore: Update dataset names and job types in upload and create scripts

--- src/scripts/00_upload_raw_game_records.py
+++ src/scripts/00_upload_raw_game_records.py
@@ -2,13 +2,13 @@ import wandb
 from dotenv import load_dotenv
 from pathlib import Path
 
-DATASET_NAME = "raw_games"
-DATASET_TYPE = "game_records"
+DATASET_TYPE  = "replay-games"
+DATASET_NAME = "replay-records"
 
 def main() -> None:
     path = Path('../input/games')
     assert list(path.glob('*.json')), f"No game records found in {path} directory."
-    run = wandb.init(job_type="upload_game_records")
+    run = wandb.init(job_type="upload-replay-records")
     artifact = wandb.Artifact(DATASET_NAME, type=DATASET_TYPE)
     artifact.add_dir(str(path))
     run.log_artifact(artifact)
--- src/scripts/01_create_type_datasets.py
+++ src/scripts/01_create_type_datasets.py
@@ -1,17 +1,28 @@
 import wandb
+from llm_20q.data import build_df, build_game_records
+from pathlib import Path                        
 from dotenv import load_dotenv
 
-INPUT_DATASET_NAME = "raw_games"
-OUTPUT_DATASET_NAME = "games"
-DATASET_TYPE = "game_records"
+load_dotenv()
+
+INPUT_DATASET_NAME = "replay-records"
+OUTPUT_DATASET_NAME = "replay-dataframe"
+DATASET_TYPE = "replay-games"
 
 def main() -> None:
-    run = wandb.init(job_type="create_game_datasets")
+    run = wandb.init(job_type="create-game-datasets")
     artifact = run.use_artifact(f'{INPUT_DATASET_NAME}:latest', type=DATASET_TYPE)
     artifact_dir = artifact.download()
-    artifact = wandb.Artifact(OUTPUT_DATASET_NAME, type=DATASET_TYPE)
-    artifact.add_dir("./data/games")
-    run.log_artifact(artifact)
+    game_records = build_game_records(folder=artifact_dir)
+    save_folder = Path('../input/replay-dataframes')
+    save_folder.mkdir(exist_ok=True, parents=True)
+    for task in ["ask", "answer", "guess"]:
+        task_df = build_df(game_records, task)
+        save_path = save_folder/f"{task}.parquet"
+        task_df.to_parquet(save_path)
+        artifact = wandb.Artifact(f"{OUTPUT_DATASET_NAME}-{task}", type=DATASET_TYPE)
+        artifact.add_file(save_path.absolute(), f"{task}-df")
+        run.log_artifact(artifact)
     run.finish()
     
     
