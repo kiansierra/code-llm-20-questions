1cb173def111d71e82f6bc56caa6c57b0c72cf7d
kiansierra <kiansierra90@gmail.com>
2024-07-10 17:54:30 +0100

    configurable train embeddings

--- src/train_embeddings.py
+++ src/train_embeddings.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import hydra
 import pandas as pd
 from datasets import Dataset
@@ -27,16 +28,30 @@ def preprocess_answers(df:pd.DataFrame) -> pd.DataFrame:
     concat_df['query'] = concat_df['question'] + " " + concat_df['answer']
     return concat_df
 
-def generate_multy_questions(df:pd.DataFrame, num_samples:int=10) -> pd.DataFrame:
+def generate_multy_questions(df:pd.DataFrame, num_samples:int=15) -> pd.DataFrame:
     questions_agg = []
     for num in range(num_samples):
-        sample_df = df.groupby('keyword').sample(n=2 + num % 3,replace=False)
+        sample_df = df.groupby('keyword').sample(n=2 + num,
+                                                 replace=False,
+                                                 random_state=num)
         sample_df = sample_df.groupby('keyword').agg({'score':'mean', 'query': "\n".join, 'prompt':'first'})
         questions_agg.append(sample_df)
     
     return pd.concat(questions_agg, ignore_index=True)
 
-@hydra.main(config_path="llm_20q/configs/rag", config_name="xlm-roberta-base", version_base=None)
+def fix_prompt(model_name:str) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    
+    def fix_nomic(df: pd.DataFrame) -> pd.DataFrame:
+        df['query'] = 'query: ' + df['query']
+        df['prompt'] = 'passage: ' + df['prompt']
+        return df
+    
+    if "nomic" in model_name:
+        return fix_nomic
+    
+    return lambda df: df
+
+@hydra.main(config_path="llm_20q/configs/rag", config_name="all-MiniLM-L6-v2", version_base=None)
 def main(config: DictConfig) -> None:
     raw_config = OmegaConf.to_container(config, resolve=True)
     run = wandb.init(config=raw_config, **config.wandb_init)
@@ -55,16 +70,19 @@ def main(config: DictConfig) -> None:
                              multi_questions[["query", "prompt", "score"]]
                              ],
                             ignore_index=True)
+    
+    selected_df = fix_prompt(model_name=config.model_name_or_path)(selected_df)
     selected_df = selected_df.rename(columns={"query": "sentence1", "prompt": "sentence2"})
     train_dataset = Dataset.from_pandas(selected_df)
     train_dataset = train_dataset.shuffle(42)
     eval_df = single_question_df[["query", "prompt", "score"]]
+    eval_df = fix_prompt(model_name=config.model_name_or_path)(eval_df)
     eval_df = eval_df.rename(columns={"query": "sentence1", "prompt": "sentence2"})
     eval_df = eval_df.sample(n=1_000, random_state=42).reset_index(drop=True)
     eval_dataset = Dataset.from_pandas(eval_df)
 
     # Load a model to train/finetune
-    model = SentenceTransformer(config.model_name)
+    model = SentenceTransformer(config.model_name_or_path, trust_remote_code=True)
 
     # Initialize the CoSENTLoss
     # This loss requires pairs of text and a float similarity score as a label
@@ -80,6 +98,11 @@ def main(config: DictConfig) -> None:
         loss=loss,
     )
     trainer.train()
+    model_artifact = wandb.Artifact(**raw_config['output_artifact'])
+    model_artifact.add_dir(config.output_dir)
+    run.log_artifact(model_artifact)
+    run.finish()
+
 
 
 if __name__ == "__main__":
