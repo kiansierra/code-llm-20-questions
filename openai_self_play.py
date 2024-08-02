99e7c56a502fbb18850ae4414585161a89678dfb
kiansierra <kiansierra90@gmail.com>
2024-08-02 01:26:17 +0100

    refactor: Update openai_self_play.py to use LLM20Q agent for improved interaction with chat completions

--- openai_self_play.py
+++ openai_self_play.py
@@ -2,15 +2,13 @@ from kaggle_environments import make
 from loguru import logger
 
 from llm_20q.players.openai import OpenaiPlayer
-from llm_20q.agent import LLM20Q
+from llm_20q.agent import LLM20Q, dumb_agent_fn
 from llm_20q.rag import SentenceTransformerRag
 
 openaiplayer = OpenaiPlayer(model='gpt-3.5-turbo', max_retries=10)
 rag = SentenceTransformerRag.from_folder("../input/rag/nomic-embed-text-v1")
 agent = LLM20Q(openaiplayer, rag)
     
-def dumb_agent_fn(obs, cfg):
-    return "yes"
 
 if __name__ == "__main__":
     env = make("llm_20_questions", debug=True)
